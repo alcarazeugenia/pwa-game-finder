a5c30c3ca19a80d250b1cef356a14ab21f4ac43a
Eugenia Alcaraz <ealcaraz@altimetrik.com>
2022-04-21 16:11:03 -0500

    Removed login

--- src/components/loginPage/loginContainer/LoginContainer.js
+++ src/components/loginPage/loginContainer/LoginContainer.js
@@ -5,19 +5,21 @@ import CarouselContainer from "../carouselContainer/CarouselContainer";
 import styles from "./LoginContainer.module.css";
 const Main = React.lazy(() => import("../../main/Main"));
 const LoginContainer = () => {
-	return !sessionStorage.getItem("user") ? (
-		<>
-			<div className={styles.login}>
-				<FormContainer />
-				<CarouselContainer />
-			</div>
-			<Suspense fallback="">
-				<Main hidden={true} />
-			</Suspense>
-		</>
-	) : (
-		<Redirect from="/" to="/main" />
-	);
+	// return !sessionStorage.getItem("user") ? (
+	// 	<>
+	// 		<div className={styles.login}>
+	// 			<FormContainer />
+	// 			<CarouselContainer />
+	// 		</div>
+	// 		<Suspense fallback="">
+	// 			<Main hidden={true} />
+	// 		</Suspense>
+	// 	</>
+	// ) : (
+	// 	<Redirect from="/" to="/main" />
+	// );
+
+	return <Redirect from="/" to="/main" />;
 };
 
 export default LoginContainer;
